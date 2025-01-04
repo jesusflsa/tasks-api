f622da555d5656f2cd8ee2c29646c5e6ab1d0319
jesus <jesusflsa2004@gmail.com>
2025-01-03 20:08:59 -0500

    add: user controller

---
+++ src/main/java/com/jesusfs/tasks/domain/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.jesusfs.tasks.domain.controller;
+
+import com.jesusfs.tasks.domain.model.user.UserModel;
+import com.jesusfs.tasks.domain.model.user.dto.ResponseUserDTO;
+import com.jesusfs.tasks.domain.model.user.dto.UpdateUserDTO;
+import com.jesusfs.tasks.domain.service.user.UserService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@Controller
+@RequestMapping("/profile")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<ResponseUserDTO> getProfile(Authentication authentication) {
+        log.info("UserController::getProfile execution started.");
+        UserModel principal = (UserModel) authentication.getPrincipal();
+
+        UserModel user = userService.getUserById(principal.getId());
+        ResponseUserDTO responseDTO = new ResponseUserDTO(user.getUsername(), user.getEmail());
+        log.info("UserController::getProfile execution ended.");
+        return ResponseEntity.ok(responseDTO);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateProfile(Authentication authentication, @RequestBody @Valid UpdateUserDTO userDTO) {
+        log.info("UserController::updateProfile execution started.");
+        UserModel principal = (UserModel) authentication.getPrincipal();
+
+        log.debug("UserController::updateProfile params received: {}", userDTO);
+        UserModel user = userService.updateUser(userDTO, principal.getId());
+        ResponseUserDTO responseDTO = new ResponseUserDTO(user.getUsername(), user.getEmail());
+        log.info("UserController::updateProfile execution ended.");
+        return ResponseEntity.ok(responseDTO);
+    }
+}
