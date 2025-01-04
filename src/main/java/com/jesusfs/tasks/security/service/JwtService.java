b98eafb328145f6ab183fa4e26bb0f078bdd1c87
jesus <jesusflsa2004@gmail.com>
2025-01-04 02:41:21 -0500

    update: set usermodel instead of userdetails

--- src/main/java/com/jesusfs/tasks/security/service/JwtService.java
+++ src/main/java/com/jesusfs/tasks/security/service/JwtService.java
@@ -2,11 +2,10 @@ package com.jesusfs.tasks.security.service;
 
 import com.jesusfs.tasks.domain.model.user.UserModel;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.security.core.userdetails.UserDetails;
 
 public interface JwtService {
     String createToken(UserModel user);
     boolean validateToken(String token);
     String getToken(HttpServletRequest request);
-    UserDetails getUserDetailsFromToken(String token);
+    UserModel getUserDetailsFromToken(String token);
 }
