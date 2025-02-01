528f23c3636e320746c081af5ee8d6415e694b0f
jesus <jesusflsa2004@gmail.com>
2025-02-01 15:18:14 -0500

    update: set serialization mode via dto

--- src/main/java/com/jesusfs/tasks/TasksApiApplication.java
+++ src/main/java/com/jesusfs/tasks/TasksApiApplication.java
@@ -3,9 +3,11 @@ package com.jesusfs.tasks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableCaching
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class TasksApiApplication {
 
 	public static void main(String[] args) {
